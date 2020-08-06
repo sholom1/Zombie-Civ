b89e51ca398d55569664b91e822b2a577ce36094
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2020-08-06 10:52:19 -0400

    Conversion - 2

--- Zombie Civ 3 LWRP/Assets/Scripts/EcsTesting/MoveOrderComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-[GenerateAuthoringComponent]
-public struct MoveOrderComponent : IComponentData
-{
-    public float3 TargetPosition;
-    public float Speed;
-}
